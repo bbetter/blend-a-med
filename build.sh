92777443f72c744069c8db32ee76eb09fd9187d2
Andrii <andriy.ihorowych@gmail.com>
2023-04-21 00:22:19 +0300

    Add cache to gitignore

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Get current date and time
+now=$(date +"%Y-%m-%d_%H-%M-%S")
+
 rm -rf blend-a-med.zip
 # Create a list of all .py files in the current directory
 py_files=$(ls *.py)
 
 # Create a new zip archive called "python_files.zip" and add each .py file to it
-zip blend-a-med.zip $py_files
+zip blend-a-med-${now}.zip $py_files
 
 # Print a message indicating the number of files that were added to the archive
 num_files=$(echo $py_files | wc -w)
